a18da525999078c51040cc012f1991921705eaf0
Rhythm Jain <rhythmrjain11@gmail.com>
2024-01-23 18:02:13 +0530

    I/O

---
+++ RevCSharp/RevInputs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevCSharp
+{
+    public static class RevInputs
+    {
+        public static void Inputs()
+        {
+            Console.Write("Enter your name => ");
+            string? userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}!");
+
+            Console.Read();
+            Console.ReadKey();
+
+            // all inputs are in form of string 
+            //use these methods to verify
+
+            if(string.IsNullOrEmpty(userName) ) { Console.WriteLine("NULL or Empty"); }
+
+            // it can also be a whitespace to check that use this:
+            if(string.IsNullOrWhiteSpace(userName) ) { Console.WriteLine("NULL or Empty or a WhiteSpace"); }
+        }
+    }
+}
---
+++ RevCSharp/RevOutputs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevCSharp
+{
+    public static class RevOutputs
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+
+            int a = 1;
+            int b = 2;
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+
+            Console.WriteLine($"{a} + {b} = {a+b}");
+
+            Console.Write("Enter Age => ");
+        }
+    }
+}
